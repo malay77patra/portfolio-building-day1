ddf1f2896dcd9b30687270c134855f0addb4a9fe
malay77patra <malay77patra@gmail.com>
2024-07-30 21:03:18 +0530

    skills added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Link, Element } from 'react-scroll';
 import SocialMedia from './components/SocialMedia';
+import Progress from './components/Progress';
 import Malay from './assets/malay.png'
 import './styles/App.css';
 
@@ -39,13 +40,13 @@ export default function App() {
           <Element name="about">
             <div className='heading'>ABOUT</div>
             <p>I'm a <span className='h'>Python expert</span>. I build web apps and automation software using the <span className="h">MERN stack</span> & Python. I also integrate <span className="h">AI & GPT</span> like LLm or APIs into prebuilt softwarews, focusing on solving problems that businesses face. Over the past two years, I have been working as a freelancer, including a notable stint with Vino AI. I possess strong skills in mathematics and physics, which complement my technical expertise. I am actively seeking new opportunities to leverage my skills and help more businesses overcome their challenges.</p>
-            <p>Get your issues solved now 😎</p>
+            <p>Get your issues solved now ✨</p>
             <button className='go-btn'>LET'S GO</button>
           </Element>
           <Element name="experties">
-            <div className='heading'>ABOUT</div>
-            <p>Back in 2020, I decided to try my hand at creating custom Tumblr themes and tumbled head first into the rabbit hole of coding and web development. Fast-forward to today, and I have had the privilege of building software for start-up, for Local Buinsesses, also helped multiple clients working as a Freelancer</p>
-            <p>My main focus these days is building products and leading projects for our clients at Upwork. In my free time I have also like to contribue Open-source projects and learning new skills.</p>
+            <div className='heading'>EXPERTIES</div>
+            <p>My focus is on building software that simplifies and grows your business. Whether you need a landing page, a complex dashboard, or task automation, I am here to help. My skills are:</p>
+            <Progress />
           </Element>
           <Element name="projects">
             <div className='heading'>ABOUT</div>
